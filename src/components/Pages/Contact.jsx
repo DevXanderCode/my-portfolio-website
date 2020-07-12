2e2843b32baef7a9039257ef616094fd50fe8239
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-07-12 13:57:48 +0100

    finished the validating the contact form with Yup validation Schema

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import {withFormik} from "formik";
+import * as Yup from "yup";
 
 //re-useable component
 import Field from "../common/Field"; 
@@ -96,15 +97,30 @@ export default withFormik({
     phone: "",
     message: "",
   }),
-  validate: (values) => {
-    const errors = {};
-    Object.keys(values).map((value) => {
-      if(!values[value]){
-        errors[value] = `The ${value} field is Required`;
-      }
-    })
-    return errors;
-  },
+  // validate: (values) => {
+  //   const errors = {};
+  //   Object.keys(values).map((value) => {
+  //     if(!values[value]){
+  //       errors[value] = `The ${value} field is Required`;
+  //     }
+  //   })
+  //   return errors;
+  // },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().min(3, "your name is longer than that")
+              .required("please the name field cannot be empty"),
+    email: Yup.string()
+              .email("Please enter a valid email")
+              .required("Please the Email field is Required"),
+    phone: Yup.string()
+              .required("Please the Phone number field can't be empty")
+              .matches(/^[0-9]+$/, "must be numbers")
+              .min(10, "please your phone number can't be below 10 digits")
+              .max(15, "Please your phone number should not be more than 15 digits"),
+    message: Yup.string()
+                .min(500, "please provide us with more detailed information")
+                .required("Please the message field is required")
+  }),
   handleSubmit: (values, {setSubmitting}) => {
     alert(JSON.stringify(values));
     console.log("values", values);
