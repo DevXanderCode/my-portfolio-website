2710c53c4ee61f546982efa7f90b0dae517570a5
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-07-25 11:34:42 +0100

    finished the admin login form

--- src/components/AdminWrapper.jsx
+++ src/components/AdminWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AdminWrapper = (props) => {
     return (
-        <div id="admin-page" style={{backgroundColor: "#eee", minHeight: "100vh"}}>
+        <div id="admin-page" style={{backgroundColor: "#eee", minHeight: "100vh", paddingTop: "6rem"}}>
             {props.children}
         </div>
     );
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -12,7 +12,16 @@ const loginPageStyle = {
     width: "50%" ,margin: "auto", 
     border: "1px groove rgba(122,122, 122, 0.4)", 
     backgroundImage: "linear-gradient(180deg, #555,rgba(118,118,118, .8))",
-    boxShadow: "0px 0px 15px rgba(0,0,0,0.5)" 
+    boxShadow: "0px 0px 15px rgba(0,0,0,0.5)" ,
+    borderRadius: "5px"
+};
+
+const formTitle = {
+    textAlign: "center", 
+    color: "white",
+    width: "100%",
+    marginBottom: "2.4rem",
+    fontSize: "2rem"
 };
 
 const Login = ({handleChange, handleSubmit, handleBlur, touched, errors, values }) => {
@@ -20,7 +29,7 @@ const Login = ({handleChange, handleSubmit, handleBlur, touched, errors, values 
         <div className="login-page" style={{ ...loginPageStyle }}>
             <div className="container">
                 <div className="login-form" style={{ padding: "1rem"}}>
-                    <h1 style={{textAlign: "center", color: "white"}}>Login</h1>
+                    <h1 style={{...formTitle}}>Login</h1>
                         <form >
                             {fields.map((field, idx) => (
                                 <Field {...field} 
