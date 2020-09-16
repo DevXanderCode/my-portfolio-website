02703cf6cb0512b75b013370d0dfce7179ffed24
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-16 13:21:38 +0100

    started designing the ui of the dashboard

--- src/components/Pages/Dashboard.jsx
+++ src/components/Pages/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const Dashboard = ({ auth: { token } }) => {
   return (
     <div style={{ padding: "0", margin: "0" }}>
+      <AppBar>
+        <Toolbar>
+          <Typography component='h1' variant='h6' color='inherit' noWrap>
+            Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <h2>You are Logged in with token {token}</h2>
     </div>
   );
