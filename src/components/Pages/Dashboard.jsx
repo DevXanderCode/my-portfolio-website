31290244456f1fbfca3be2e0ecc9d5ecea7ae0d7
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-16 16:32:06 +0100

    added a bit of animation to the sidebar

--- src/components/Pages/Dashboard.jsx
+++ src/components/Pages/Dashboard.jsx
@@ -24,10 +24,18 @@ const styles = (theme) => ({
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   drawerPaper: {
     width: drawerWidth,
