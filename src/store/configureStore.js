15bf03c5fd8ca56730e55534596422f7a659556e
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-17 07:33:38 +0100

    started creating configure store

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
