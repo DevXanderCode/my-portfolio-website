08c5ae6934990b70627fd967c367bcf8ab854d76
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-17 07:54:23 +0100

    persisted the store

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./store/reducers";
+// import { createStore, applyMiddleware } from "redux";
+// import rootReducer from "./store/reducers";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./store/configureStore";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+const { store, persistor } = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
