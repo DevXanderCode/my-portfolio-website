d90a9f6a7fe259215b4df40f938a802ccd80ae14
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-17 15:33:38 +0100

    started integrating the table view into the admin post component

--- src/components/Pages/Admin/Posts.jsx
+++ src/components/Pages/Admin/Posts.jsx
@@ -4,13 +4,31 @@ import { connect } from "react-redux";
 import TableView from "../../common/TableView";
 import * as AdminActions from "../../../store/actions/adminActons";
 
-const Posts = (props) => {
+const Posts = ({ auth, ...props }) => {
+  const columns = [
+    { label: "ID", name: "id" },
+    { label: "Title", name: "title" },
+  ];
   return (
     <div style={{ padding: "0", margin: "0" }}>
       <h4>Testing Posts</h4>
-      <TableView columns={[]} rows={[]} />
+      <TableView columns={columns} rows={[]} />
     </div>
   );
 };
 
-export default withRouter(Posts);
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPosts: (token) => {
+      dispatch(AdminActions.getPosts(token));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Posts));
