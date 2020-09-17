bffa87542030486b65c8973d12ec42f3cfecb42b
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-17 15:16:35 +0100

    created the get post action

--- src/components/Pages/Admin/Users.jsx
+++ src/components/Pages/Admin/Users.jsx
@@ -1,31 +1,39 @@
 import * as React from "react";
-import TableView from "../../common/TableView";
 import { connect } from "react-redux";
+import TableView from "../../common/TableView";
+import * as AdminActions from "../../../store/actions/adminActons";
 
-const Users = (props) => {
+const Users = ({ auth, getUsers, admin: { user }, ...props }) => {
   const columns = [
     { label: "ID", name: "id" },
     { label: "Email", name: "email" },
     { label: "Name", name: "name" },
   ];
 
+  React.useEffect(() => {
+    getUsers(auth.token);
+  }, []);
   return (
     <div style={{ padding: "0", margin: "0" }}>
       <h1> testing Users</h1>
-      <TableView columns={columns} rows={[]} />
+      <TableView columns={columns} rows={user} />
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    auth: state.auth,
+    admin: state.admin,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    getUsers: (token) => {
+      dispatch(AdminActions.getUsers(token));
+    },
+  };
 };
 
-export default connect({
-  mapStateToProps,
-  mapDispatchToProps,
-})(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
--- src/store/actions/adminActons.js
+++ src/store/actions/adminActons.js
@@ -10,3 +10,14 @@ export const getUsers = (token) => {
     });
   };
 };
+
+export const getPosts = (token) => {
+  return (dispatch) => {
+    API.getPosts(token, (res) => {
+      dispatch({
+        type: "GOT_POSTS",
+        payload: res.data,
+      });
+    });
+  };
+};
