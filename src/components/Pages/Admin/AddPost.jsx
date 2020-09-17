a34e3fd853e62f152ecc6abff8a1e35e05bfa6cd
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-17 21:08:55 +0100

    started working on the add Post components form

--- src/components/Pages/Admin/AddPost.jsx
+++ src/components/Pages/Admin/AddPost.jsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import * as AdminActions from "../../../store/actions/adminActons";
+import {
+  paper,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
+} from "@material-ui/core";
 
 const AddPost = ({ ...props }) => {
   return (
