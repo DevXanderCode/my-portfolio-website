01a05ef81e13dcc0105ff7fe73141169205b11c1
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-18 09:01:18 +0100

    created the title, slug and content input fields

--- src/components/Pages/Admin/AddPost.jsx
+++ src/components/Pages/Admin/AddPost.jsx
@@ -1,25 +1,61 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import {
-  paper,
-  FormControl,
-  Input,
-  InputLabel,
-  FormHelperText,
-} from "@material-ui/core";
+import { paper } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-import { withFormik } from "formik";
+import { withFormik, Form } from "formik";
+import { FormikTextField } from "formik-material-fields";
 import * as YUP from "yup";
 import * as AdminActions from "../../../store/actions/adminActons";
 
 const styles = (theme) => ({
-  container: {},
+  container: {
+    margin: theme.spacing(3),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+  },
 });
 
-const AddPost = ({ classes, ...props }) => {
+const AddPost = ({
+  classes,
+  values,
+  setFieldValue,
+  setFieldTouched,
+  ...props
+}) => {
   return (
     <div className={classes.container}>
+      {console.log("values: ", values.title)}
       <h1>Add Posts</h1>
+      <Form>
+        <FormikTextField
+          name='title'
+          label='Title'
+          margin='normal'
+          onChange={(e) => {
+            return (
+              setFieldValue(
+                "slug",
+                e.target.value.toLowerCase().replace(/ /g, "_")
+              ),
+              setFieldTouched("slug", true, false)
+            );
+          }}
+          fullWidth
+        />
+        <FormikTextField
+          name='slug'
+          // label='Slug'
+          placeholder='Slug'
+          margin='normal'
+        />
+        <FormikTextField
+          name='content'
+          label='content'
+          margin='normal'
+          fullWidth
+        />
+      </Form>
     </div>
   );
 };
@@ -41,9 +77,12 @@ export default connect(
       slug: "",
       createdAt: "",
       status: false,
+      content: "",
     }),
     validationSchema: YUP.object().shape({
-      // title: YUP.string,
+      title: YUP.string().required("Post Title is Requiured"),
+      slug: YUP.string().required(),
+      content: YUP.string().required(),
     }),
     handleSubmit: (values, { setSubmittind }) => {},
   })(withStyles(styles)(AddPost))
