6382341b9f0b8bc675f1fdb180bdb791fa1aacc1
Alex Nweke <nwekealexchinedu@gmail.com>
2020-10-06 00:32:55 +0100

    fixed the table view component issue

--- src/components/common/TableView.jsx
+++ src/components/common/TableView.jsx
@@ -1,41 +1,28 @@
-import * as React from "react";
-import { withSytles } from "@material-ui/core/styles";
-import {
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  Paper,
-} from "@material-ui/core";
+import * as React from 'react';
+import { withSytles } from '@material-ui/core/styles';
+import { Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core';
 
 const TableView = ({ rows, columns }) => {
-  return (
-    <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {columns
-              ? columns.map((col, i) => (
-                  <TableCell key={i}>{col.label}</TableCell>
-                ))
-              : null}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            {rows
-              ? rows.map((row, i) =>
-                  columns.map((col, idx) => (
-                    <TableCell key={idx}>{row[col.name]}</TableCell>
-                  ))
-                )
-              : null}
-          </TableRow>
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+	return (
+		<Paper>
+			<Table>
+				<TableHead>
+					<TableRow>
+						{columns ? columns.map((col, i) => <TableCell key={i}>{col.label}</TableCell>) : null}
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows ? (
+						rows.map((row, i) => (
+							<TableRow>
+								{columns.map((col, idx) => <TableCell key={idx}>{row[col.name]}</TableCell>)}
+							</TableRow>
+						))
+					) : null}
+				</TableBody>
+			</Table>
+		</Paper>
+	);
 };
 
 export default TableView;
